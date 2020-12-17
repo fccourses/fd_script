d27cb35d0812fa275e4d866f4a27b6b577191345
Vladimir <freshcode.yt@gmail.com>
2020-12-17 15:06:07 +0200

    Add vars

---
+++ index.js
@@ -0,0 +1,12 @@
+let box; //Объявление переменной
+
+box = 10; //Инициализация переменной
+
+console.log("Я джава скрипт девеопер!");
+
+console.log( box ); // Логгирование переменной
+
+console.log(undefined);
+console.log(null);
+console.log(true);
+console.log(false);
