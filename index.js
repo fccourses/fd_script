1fe82453ab6cd98614e07a7a7b89042adf454ce1
Vladimir <freshcode.yt@gmail.com>
2020-12-21 18:50:47 +0200

    Add userInput

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-console.log(1);
+const userInput1 = prompt("Введите число 1:");
+const userInput2 = prompt("Введите число 2:");
 
-const result = false;
+const userNumber1 = +userInput1;
+const userNumber2 = +userInput2;
 
-if (result) {
-  console.log("ok");
+const result = userNumber1 + userNumber2;
+
+const verdict = isNaN(result);
+
+if (verdict) {
+  alert("Вы ввели не числа");
 } else {
-  console.log("not ok");
+  alert(result);
 }
-
-console.log(2);
