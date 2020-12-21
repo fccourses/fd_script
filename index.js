18abc526c3f7a07efa99fe4642ae5ba7b29d2f07
Vladimir <freshcode.yt@gmail.com>
2020-12-21 19:10:36 +0200

    Add function expression

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const userInput1 = prompt("Введите число 1:");
-const userInput2 = prompt("Введите число 2:");
+/* const userInput1 = +prompt("Введите число 1:");
+const userInput2 = +prompt("Введите число 2:");
 
-const userNumber1 = +userInput1;
-const userNumber2 = +userInput2;
+const result = userInput1 + userInput2;
 
-const result = userNumber1 + userNumber2;
-
-const verdict = isNaN(result);
-
-if (verdict) {
+if (isNaN(result)) {
   alert("Вы ввели не числа");
 } else {
   alert(result);
-}
+} */
+
+const myFirstFunc = function () {
+  console.log("1");
+};
+
+myFirstFunc();
