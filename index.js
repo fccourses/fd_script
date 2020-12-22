c792c5eb9f67eca95f9b1499cc9148266bc4d60e
Vladimir <freshcode.yt@gmail.com>
2020-12-22 16:54:08 +0200

    Add calc solved

--- index.js
+++ index.js
@@ -11,20 +11,36 @@ const division = function (a, b) {
   return a / b;
 };
 
-const userInput1 = prompt('введите 1 число');
-const userInput2 = prompt('введите 2 число');
-const operation = prompt('Введите знак операции');
-
 const calculate = function (a, b, sign) {
-  if (!isNaN(userInput1)) {
-    if (!isNaN(userInput2)) {
-      /* 
-      YOUR CODE ARE HERE 
-      */
+  const check = a - b;
+  if (isNaN(check)) {
+    return null;
+  }
+
+  switch (sign) {
+    case '/': {
+      const result = division(a, b);
+      return result;
+    }
+    case '*': {
+      return mul(a, b);
+    }
+    case '-': {
+      return subtraction(a, b);
+    }
+    case '+': {
+      return sum(a, b);
+    }
+    default: {
+      return null;
     }
   }
 };
 
-const result = calculate( /* передать сюда 2 числа и знак операции */ );
+const userInput1 = +prompt('введите 1 число');
+const userInput2 = +prompt('введите 2 число');
+const operation = prompt('Введите знак операции');
+
+const result = calculate(userInput1, userInput2, operation);
 
 console.log(result);
