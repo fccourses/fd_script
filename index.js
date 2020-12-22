c396e2a75ee441bb4c425d3092c9420417ef737b
Vladimir <freshcode.yt@gmail.com>
2020-12-22 16:12:31 +0200

    Add task template
    Write simple calculator with this template

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const userInput = +prompt('Выберите позицию меню:\n1-Чай\n2-кофе\n3-капучино');
+const sum = function (a, b) {
+  return a + b;
+};
+const subtraction = function (a, b) {
+  return a - b;
+};
+const mul = function (a, b) {
+  return a * b;
+};
+const division = function (a, b) {
+  return a / b;
+};
 
-switch (userInput) {
-  case 1: {
-    console.log('чай');
-    break;
-  }
-  case 2: {
-    console.log('кофе');
-    break;
-  }
-  case 3: {
-    console.log('капучино');
-    break;
-  }
-  default: {
-    console.log('Попробуйте ещё раз');
-  }
-}
+const userInput1 = prompt('введите 1 число');
+const userInput2 = prompt('введите 2 число');
+const operation = prompt('Введите знак операции');
 
-console.log('end of script');
+const calculate = function (a, b, sign) {
+  if (!isNaN(userInput1)) {
+    if (!isNaN(userInput2)) {
+      /* 
+      YOUR CODE ARE HERE 
+      */
+    }
+  }
+};
+
+const result = calculate( /* передать сюда 2 числа и знак операции */ );
+
+console.log(result);
