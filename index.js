ec2c84fcbc4fe5a235be445f2f3f2daa5498b749
Vladimir <freshcode.yt@gmail.com>
2020-12-22 16:01:26 +0200

    Add menu selector

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const userInput = +prompt('Выберите позицию меню:\n1-Чай\n2-кофе\n3-капучино');
 
-if (isNaN(userInput)) {
-} else if (userInput === 1) {
-  console.log('Вы выбрали чай');
-} else if (userInput === 2) {
-  console.log('Вы выбрали кофе');
-} else if (userInput === 3) {
-  console.log('Вы выбрали Капучино');
-} else {
-  console.log('Попробуйте ещё раз');
+switch (userInput) {
+  case 1: {
+    console.log('чай');
+    break;
+  }
+  case 2: {
+    console.log('кофе');
+    break;
+  }
+  case 3: {
+    console.log('капучино');
+    break;
+  }
+  default: {
+    console.log('Попробуйте ещё раз');
+  }
 }
 
 console.log('end of script');
