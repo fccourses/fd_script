f34fdfc32a04dc52ba50db955cabd7e4bac59ff8
Vladimir <freshcode.yt@gmail.com>
2020-12-23 17:04:51 +0200

    Add for loop

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict';
 
-
 /* 
   1. Спрашивать у пользователя пароль до тех пор,
   пока он не введёт правильный пароль,
   1.2 Ограничить число попыток ввода до 5.
 */
 
-let count = 1;
-
+/* let count = 1;
 while (count <= MAX_TRY) {
   count++;
+  const userPass = prompt('Password: ');
+
+  if (GOOD_PASSWORD === userPass) {
+    console.log('Пароль верный');
+    break;
+  }
+} */
+/*       начало;        условие;        шаг */
+for (let count = 1; count <= MAX_TRY; count++) {
   const userPass = prompt('Password: ');
 
   if (GOOD_PASSWORD === userPass) {
@@ -21,4 +28,7 @@ while (count <= MAX_TRY) {
 
 /* 
   2. Найти факториал числа
- */
+*/
+/* 
+  3. Функция возведения числа в степень. Использовать цикл.
+*/
