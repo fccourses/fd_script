0aee23a4adf249567e6c6f2a5c7f44cb4ec51efb
Vladimir <freshcode.yt@gmail.com>
2020-12-23 15:16:01 +0200

    Add while loop

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 'use strict';
 
-function sum(a, b) {
-  return a + b;
+let count = 0;
+
+while (count++ < 5) {
+  console.log(count);
 }
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function highOrderFunc(num1, num2, funcToPerformOperation) {
-  const result = funcToPerformOperation(num1, num2);
-  return result;
-}
-
-const res1 = highOrderFunc(10, 20, subtract);
-
-console.log(res1);
