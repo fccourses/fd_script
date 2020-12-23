65f6f4ffe3b1ea7b60beac80d2cc4dc1d12174a0
Vladimir <freshcode.yt@gmail.com>
2020-12-23 14:18:03 +0200

    Add ternary

--- index.js
+++ index.js
@@ -1,56 +1,13 @@
-const sum = function (a, b) {
-  return a + b;
-};
-const subtraction = function (a, b) {
-  return a - b;
-};
-const mul = function (a, b) {
-  return a * b;
-};
-const division = function (a, b) {
-  return a / b;
-};
-/**
- * 
- * @param {number} a 
- * @param {number} b 
- * @param {string} sign 
- * @returns {null | number}
- */
-const calculate = function (a, b, sign) {
-  const check = a - b;
-  if (isNaN(check)) {
-    return null;
+const getUserInput = function () {
+  const userInput = prompt('Чтото введите:');
+
+  /*   if (userInput) {
+    return userInput;
   }
-  let f = 1;
-  switch (sign) {
-    case '/': {
-      f = division;
-      break;
-    }
-    case '*': {
-      f = mul;
-      break;
-    }
-    case '-': {
-      f = subtraction;
-      break;
-    }
-    case '+': {
-      f = sum;
-      break;
-    }
-  }
-  if (typeof f === 'function') {
-    return f(a, b);
-  }
-  return null;
+  return 'Вы ничего не ввели'; */
+  return userInput ? userInput : 'Вы ничего не ввели';
 };
 
-const userInput1 = +prompt('введите 1 число');
-const userInput2 = +prompt('введите 2 число');
-const operation = prompt('Введите знак операции');
-
-const result = calculate(userInput1, userInput2, operation);
+const result = getUserInput();
 
 console.log(result);
