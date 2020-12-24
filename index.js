033dfb5357773204164ae8c67ec79678914e7c68
Vladimir <freshcode.yt@gmail.com>
2020-12-24 15:05:44 +0200

    Add cat object

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const object1 = {}; // литерально
-const object2 = Object();
-const object3 = new Object();
+//Создали объект кота
+const cat = {
+  color: 'black',
+  breed: 'Spinx',
+  age: 8,
+  isMale: true,
+  name: 'Kasya',
+};
 
+console.log(cat);
+cat.age = cat.age + 1; // изменили существующее свойство на основе старого значения
+console.log(cat);
+cat.name = 'Test Testovich'; // изменили существующее свойство
+console.log(cat);
+cat.amountOfLegs = 4; // создали новое свойство
+console.log(cat);
 
+delete cat.amountOfLegs // delete property
+console.log(cat);
