d20edce08cca3086752c2c8c35642206faeb74fd
Vladimir <freshcode.yt@gmail.com>
2020-12-28 16:42:14 +0200

    Add computed property

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const PROTOCOL = 'http://';
-const SERVER_IP = '127.0.0.1';
-const PORT = 3000;
+const user = {
+  'favorite color': 'red',
+  0: 'test value',
+  1: 'test value 2',
+};
 
-function createQuery(path) {
-  const url = `${PROTOCOL}${SERVER_IP}:${PORT}${path}`
-  return url;
-}
+// alert(user.favorite color);
+
+console.log(user[0]);
+console.log(user['0']);
+
+const test = '1';
+
+console.log(user[test]);
+
