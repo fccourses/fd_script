63e3b5757765923d9cfc36cb25fc6e4d2293b7f2
Vladimir <freshcode.yt@gmail.com>
2020-12-30 14:56:24 +0200

    Add User

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 'use strict';
 
-/* 
-
-Напишите функцию-конструктор Accumulator(startingValue).
-
-Объект, который она создаёт, должен уметь следующее:
-
-Хранить «текущее значение» в свойстве value. 
-Начальное значение устанавливается в аргументе конструктора startingValue.
-Метод read() использует prompt для получения числа и прибавляет его к свойству value.
-Таким образом, свойство value является текущей суммой всего,
- что ввёл пользователь при вызовах метода read(),
-  с учётом начального значения startingValue.
-
-*/
-
-function Accumulator(startingValue) {
-  this.read = function read() {
-    const userNumber = +prompt('Enter number');
-    if (this.checkNumber(userNumber)) {
-      return null;
-    }
-    this.value += userNumber;
-    return this.value;
-  };
-
-  this.checkNumber = function (number) {
-    return isNaN(number) || number < 0;
-  };
-
-  if (this.checkNumber(startingValue)) {
-    return null;
-  }
-  this.value = startingValue;
+function User(firstName, lastName, age, isMale) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
 }
+
+function UserPrototype() {
+  this.toString = function toString() {
+    return `${this.firstName} ${this.lastName}`;
+  };
+
+  this.sayHello = function sayHello() {
+    return `User: ${this.firstName} says "Hello!"`;
+  };
+  /* 
+  Написать метод say, который принимает строку и вызвращает
+    строку. Формат возвращаемой строки:
+    `<Полное имя пользователя> says:
+    "<содержимое принятой строки>"`
+  */
+  this.say = function say(string) {
+    debugger;
+    if (string) {
+      return `${this.firstName} ${this.lastName} says:\n${string}`;
+    }
+  };
+}
+User.prototype = new UserPrototype();
+
+const u1 = new User('Test', 'Testovich', 18, true);
+const u2 = new User('Vlad', 'Testovich', 28, true);
+
+u1.sayHello();
+u2.sayHello();
