a6061b703b72e9e1e850e64eea6da62a5eea49ba
Vladimir <freshcode.yt@gmail.com>
2021-01-05 14:48:12 +0200

    Add HOF example

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
+/**
+ *
+ * @param {function} howToSay
+ * @param {string} whatToSay
+ */
+function saySomething(howToSay, whatToSay) {
+  howToSay(whatToSay); //=== alert(whatToSay);
+}
 
+saySomething(alert, 'Welcome!!');
