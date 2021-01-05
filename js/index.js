76ee520407f0b02b28e8d88286faa38e2c0488ef
Vladimir <freshcode.yt@gmail.com>
2021-01-05 16:21:05 +0200

    Add createRandomUsers

--- js/index.js
+++ js/index.js
@@ -8,5 +8,18 @@ function User(firstName, lastName, age, isMale, isSubscribed = false) {
   this.isSubscribed = isSubscribed;
 }
 
+function createRandomUsers(amount = 1) {
+  const db = [];
 
+  for (let i = 0; i < amount; i++) {
+    const user = new User(
+      `Name${i+1}`,
+      `Surname${i+1}`,
+      Math.ceil(Math.random() * 100),
+      Math.random() > 0.5
+    );
+    db.push(user);
+  }
 
+  return db;
+}
