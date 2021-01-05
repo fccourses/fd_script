8a22822f005accbf7500f012e9158e97740bdba3
Vladimir <freshcode.yt@gmail.com>
2021-01-05 16:57:11 +0200

    Add some examples

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,4 @@
 const GOOD_PASSWORD = 'qwerty123';
 const MAX_TRY = 5;
 const MAX_SAFE_FACTORIAL = 200;
+const ADULT_AGE = 18;
--- js/index.js
+++ js/index.js
@@ -1,25 +1,49 @@
 'use strict';
 
+function getFullName() {
+  return `${this.firstName} ${this.lastName}`;
+}
 function User(firstName, lastName, age, isMale, isSubscribed = false) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.age = age;
   this.isMale = isMale;
   this.isSubscribed = isSubscribed;
+  this.getFullName = getFullName;
 }
-
 function createRandomUsers(amount = 1) {
   const db = [];
 
   for (let i = 0; i < amount; i++) {
     const user = new User(
-      `Name${i+1}`,
-      `Surname${i+1}`,
+      `Name${i + 1}`,
+      `Surname${i + 1}`,
       Math.ceil(Math.random() * 100),
       Math.random() > 0.5
     );
     db.push(user);
   }
-
   return db;
 }
+const users = createRandomUsers(100);
+/* Получить массив полных имён с помощью метода map */
+const getNames = function (user) {
+  return user.getFullName(); // Full Name
+};
+
+const userNames = users.map(getNames);
+
+/* Получить массив совершеннолетних пользователей (filter) */
+
+const adultUsers = users.filter(function (user) {
+  return user.age >= ADULT_AGE;
+});
+
+console.table(adultUsers);
+
+/* 1.1 Зарандомить подписку для каждого пользователя.
+  1.2Получить массив совершеннолетних пользователей 
+женского пола.
+  2.1 Добавить Почту для пользователей.
+  2.2 Как 1.2, но только их почты.
+*/
