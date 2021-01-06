ffa4a7778abaac167fbea6ac1dfb367f30e029ef
Vladimir <freshcode.yt@gmail.com>
2021-01-06 14:12:17 +0200

    Add some tasks

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@
 function getFullName() {
   return `${this.firstName} ${this.lastName}`;
 }
-function User(firstName, lastName, age, isMale, isSubscribed = false) {
+function User(firstName, lastName, email, age, isMale, isSubscribed = false) {
   this.firstName = firstName;
   this.lastName = lastName;
+  this.email = email;
   this.age = age;
   this.isMale = isMale;
   this.isSubscribed = isSubscribed;
@@ -16,10 +17,12 @@ function createRandomUsers(amount = 1) {
 
   for (let i = 0; i < amount; i++) {
     const user = new User(
-      `Name${i + 1}`,
-      `Surname${i + 1}`,
-      Math.ceil(Math.random() * 100),
-      Math.random() > 0.5
+      `Name${i + 1}`, // firstName
+      `Surname${i + 1}`, //lastName
+      `email${i + 1}@mail.com`, //email
+      Math.ceil(Math.random() * 100), //age
+      Math.random() > 0.5, // isMale
+      Math.random() > 0.5 // isSubscribed
     );
     db.push(user);
   }
@@ -39,11 +42,27 @@ const adultUsers = users.filter(function (user) {
   return user.age >= ADULT_AGE;
 });
 
-console.table(adultUsers);
-
-/* 1.1 Зарандомить подписку для каждого пользователя.
+/* 
+  1.1 Зарандомить подписку для каждого пользователя.
   1.2Получить массив совершеннолетних пользователей 
 женского пола.
   2.1 Добавить Почту для пользователей.
   2.2 Как 1.2, но только их почты.
 */
+const adultWoman = users.filter(function (user) {
+  return user.age >= ADULT_AGE && !user.isMale;
+});
+
+const adultWomanEmails = adultWoman.map(function (user) {
+  return user.email;
+});
+
+//ladder.up().up()
+
+const mails = users
+  .filter(function (user) {
+    return user.age >= ADULT_AGE && !user.isMale;
+  })
+  .map(function (user) {
+    return user.email;
+  });
