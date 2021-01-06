cec884f38433f048768c198e0cda94662e087376
Vladimir <freshcode.yt@gmail.com>
2021-01-06 14:57:08 +0200

    Add square arrow function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
 'use strict';
 
-const newsPaper = {
-  title: 'NY Times',
-  articles: ['String1', 'News1', 'Test1', 'Headiubng1'],
-
-  showArticles() {
-    console.log(this);
-    this.articles.forEach( (currentArticle, index) => {
-      console.log(this); // newsPaper
-      console.log(`${this.title} ${index + 1}: ${currentArticle}`);
-    });
-  },
-};
-
-newsPaper.showArticles();
+const square = a => a * a;
