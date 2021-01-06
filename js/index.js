ebdd3d129253f8b817e52f57cca137d82d1dcf55
Vladimir <freshcode.yt@gmail.com>
2021-01-06 16:43:12 +0200

    Add logNum

--- js/index.js
+++ js/index.js
@@ -19,6 +19,15 @@ function power(num, exp) {
   return num * power(num, exp - 1);
 }
 
+/* Залогировать все числа от 1 до N */
+
+function logNum(n) {
+  if (n === 1) {
+    return 1; //basis
+  }
+  return `${logNum(n - 1)} ${n}`;
+}
+
 /*
 Этапы рекурсии:
   1) Происходит рекурсивный вызов функций
