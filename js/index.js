19dd920f50f3633d3fdf807e26c3faa9e4e710e7
Vladimir <freshcode.yt@gmail.com>
2021-01-06 15:02:18 +0200

    Add context solution 2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-const square = a => a * a;
+const newsPaper = {
+  title: 'NY Times',
+  articles: ['String1', 'News1', 'Test1', 'Headiubng1'],
+
+  showArticles() {
+    const that = this; 
+    // сохраняем сслыку на этот контекст
+
+    this.articles.map(function (value) {
+      console.log(`${that.title}: ${value}`);
+    });
+  },
+};
+
+newsPaper.showArticles();
