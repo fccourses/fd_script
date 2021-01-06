f364be4bff45f0579cf3ba49f35e912c8679ebb2
Vladimir <freshcode.yt@gmail.com>
2021-01-06 16:03:16 +0200

    Add arr concat without concat

--- js/index.js
+++ js/index.js
@@ -13,8 +13,24 @@ function test2(a, b, ...args) {
 Стрелочная функция суммирования 
 бесконечного кол-ва чисел 
 */
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers2 = [10, 12, 1435, 345, 345, 346, 8];
+/* Склеить два массива без concat */
+const res = [...numbers, ...numbers2]; //concat без concat
+
 const sum = (...args) => {
-  return args.reduce((result, value) => {
-    return result + value;
-  });
+  /*  
+ VERSION 1
+ let acc = null;
+
+  args.forEach((element) => {
+    acc += element;
+  }); 
+
+  return acc;*/
+
+  return args.reduce((result, value) => result + value);
 };
+
+console.log(sum(...numbers)); //spread(разбор) оператор.
+sum(...numbers2, ...numbers);
