d08ec8da6e9bf2fc9ffe54bc13fcd04354c158ca
Vladimir <freshcode.yt@gmail.com>
2021-01-11 17:01:22 +0200

    Add throw example

--- js/index.js
+++ js/index.js
@@ -1,35 +1,12 @@
 'use strict';
 
-function toUpperCase(str) {
-  let newStr = '';
+const userInput = prompt('Введите число');
 
-  for (let i = 0; i < str.length; i++) {
-    newStr += str[i].toUpperCase();
+console.log(square(userInput));
+
+function square(a) {
+  if( typeof a !== 'number' ){
+   throw new TypeError('Тип не тот')
   }
-
-  return newStr;
+  return a * a;
 }
-
-function toJadenCase(str) {
-  return str
-    .split(' ')
-    .map((word) => {
-      const letterArr = [...word];
-      letterArr[0] = letterArr[0].toUpperCase();
-      return letterArr.join('');
-    })
-    .join(' ');
-}
-
-// 'test tes ttest' => 'Test Tes Ttest'
-
-/* 
-Посчитать кол-во гласных, которые содержаться в строке
-Из функции вернуть их кол-во 
-Гласными являются «a», «e», «i», «o», «u».
-*/
-const countVowels = (string) =>
-  string
-  .split('')
-  .filter((char) => ['a', 'e', 'i', 'o', 'u'].includes(char))
-  .length;
