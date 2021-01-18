33ac19a77116599e6cf8b59e27913cf8eb57bf5f
Vladimir <freshcode.yt@gmail.com>
2021-01-18 16:06:49 +0200

    Add Stack

---
+++ js/Stack.js
@@ -0,0 +1,45 @@
+class Stack {
+  constructor(maxSize = 1000, ...args) {
+    this._maxSize = maxSize;
+    this._size = 0;
+
+    for (const item of args) {
+      this.push(item);
+    }
+  }
+
+  get isEmpty() {
+    return this[`_${this._size}`] === 0;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  push(value) {
+    if (this._size >= this._maxSize) {
+      throw new RangeError('Stack overflow');
+    }
+    this[`_${this._size++}`] = value;
+    return this._size;
+  }
+
+  pop() {
+    if (this.isEmpty) {
+      return;
+    }
+    const lastItem = this[`_${--this._size}`];
+    delete this[`_${this._size}`];
+    return lastItem;
+  }
+
+  pick() {
+    if (this.isEmpty) {
+      return;
+    }
+
+    return this[this._size - 1];
+  }
+}
+
+const stack = new Stack();
