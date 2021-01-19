5bae583da64af2e8b53685c46ebed55b592dcd07
Vladimir <freshcode.yt@gmail.com>
2021-01-19 16:30:14 +0200

    Add translate easy example

--- js/index.js
+++ js/index.js
@@ -1,20 +1,40 @@
-const mySymbol = Symbol();
+'use strict';
 
-const object = {
-  test: 1,
-  [mySymbol]: 2,
+const vocabulary = new Map(); // Ассоциативный массив
+vocabulary.set('cat', 'кот');
+vocabulary.set('dog', 'собака');
+vocabulary.set('search', 'поиск');
+vocabulary.set('monkey', 'обезьяна');
+vocabulary.set('good', 'хорошо');
+vocabulary.set('bad', 'плохо');
+vocabulary.set('apple', 'яблоко');
+vocabulary.set('structure', 'структура');
+vocabulary.set('algorithm', 'алгоритм');
+vocabulary.set('tree', 'дерево');
+vocabulary.set('graph', 'граф');
+vocabulary.set('object', 'объект');
+vocabulary.set('table', 'стол');
+
+const userInput = 'cAt mOnkey dOg example trEe table algorithm graph object apple good';
+
+/* 
+String: split, toLowerCase
+Array: join, map
+Map: get, has
+*/
+
+const translate = (str) => {
+
+  const lowerStr = str.toLowerCase();
+  const wordsArray = lowerStr.split(' ');
+
+  const russianWordsArray = wordsArray.map((word) => {
+    if(vocabulary.has(word)){
+      const translatedWord = vocabulary.get(word);
+      return translatedWord;
+    }
+    return word
+  })
+  const russianStr = russianWordsArray.join(' ');
+  return russianStr;
 };
-
-const myMap = new Map(); // Ассоциативный массив
-myMap.set(1, 'Value1');
-myMap.set('1', true);
-
-console.log(myMap.get(1));
-console.log(myMap.get('1'));
-
-myMap.set(1, { newProp: 'test' });
-myMap.set(object, function () {});
-
-console.log(myMap.has(1));
-console.log(myMap.has('1'));
-console.log(myMap.has('test'));
