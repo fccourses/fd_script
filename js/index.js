75594e7ad2470995e71c5d438952e21533701b4e
Vladimir <freshcode.yt@gmail.com>
2021-01-20 14:56:19 +0200

    Add task

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ vocabulary.set('tree', 'дерево');
 vocabulary.set('graph', 'граф');
 vocabulary.set('object', 'объект');
 vocabulary.set('table', 'стол');
+
 //=============================
 const keysArray = [];
 for (const key of vocabulary.keys()) {
@@ -27,12 +28,25 @@ for (const value of vocabulary.values()) {
 }
 console.log(valuesArray);
 //=============================
-
 const rusWords = [...vocabulary.values()];
 const engWords = [...vocabulary.keys()];
-
 //=============================
 
+compare('test','sett') // true
+compare('love','evol') // true
+compare('test','lore') // false
+compare('Mom','Omo') // false
+
+/* 
+  Напишите функцию, которая проверяет,
+ может ли строка быть воссоздана с
+ использованием символов другой строки.
+
+ Первый параметр функции - создаваемая строка.
+ Второй параметр - используемая строка.
+
+ Функция возвращает Boolean.
+*/
 
 const userInput =
   'cAt mOnkey dOg example trEe table algorithm graph object apple good';
